b84bb611dd29889d105786cfb98afe54cf6e248c
Nathan Ladd <nathanladd@gmail.com>
2025-05-17 13:53:47 -0400

    Tests are repaired

--- lib/test_bench/fixture.rb
+++ lib/test_bench/fixture.rb
@@ -1,5 +1,24 @@
+require 'json'
+
 require 'test_bench/fixture/dependencies'
-TestBench::Fixture::Dependencies::ImportConstants.(TestBench::Fixture::Dependencies, TestBench)
+TestBench::Fixture::Dependencies::ImportConstants.(TestBench::Fixture::Dependencies)
 
 require 'test_bench/fixture/load'
 require 'test_bench/fixture/fixture'
+
+module Test
+  module Automated
+    TestBench::Fixture::Dependencies::ImportConstants.(TestBench, self)
+  end
+end
+
+[
+  'import_constants',
+  'pseudorandom',
+  'test/automated/telemetry',
+  'test/automated/session',
+  'test/automated/output',
+  'test/automated/fixture'
+].each do |feature|
+  $LOADED_FEATURES.push(feature)
+end
